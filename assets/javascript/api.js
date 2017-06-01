17e289e0f7d31b91e99dc3a73a9ec5fd2c24becf
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-31 20:38:52 -0700

    working on QueryURL parameters.

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -9,30 +9,59 @@
 var topics = ["Movies", "TV Shows", "Entertainment", "Politics", "Science", "Military",
     "Fashion", "Music", "Pop Culture", "Sports"];
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=movies&api_key=dc6zaTOxFJmzC"
+var queryURL = "http://api.giphy.com/v1/gifs/search?rating(g pg, pg-13, r)&limit=12&q="
+var api_key = "&api_key=dc6zaTOxFJmzC"
 
 $(document).ready(function(){
     renderButtons();
+    $("#topicBtn").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var topic = $("#topic-input").val().trim();
+
+        // Adding topic from the textbox to our array
+        topics.push(topic);
+
+        // Calling renderButtons which handles the processing of our topics array
+        renderButtons();
+    });
+
+    $(".topic").click(function(){
+        var btn = $(this); //now we know what button they pressed
+        var txt = $(this).text(); //I think? Trying to get the button's text
+
+        var query = queryURL + txt + api_key;
+        console.log(query); //to test...
+        //do ajax from this point...
+        $.ajax({
+            url: query,
+            method: 'GET'
+        }).done(function(response){
+            console.log('ajax finished');
+        });
+    });
 });
 
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-    //.done(function(response) {
-    //console.log(response.data[0].embed_url);
+//
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+//     //.done(function(response) {
+//     //console.log(response.data[0].embed_url);
+//
+// }).done(function(response) {
+//     var images = $(".images");
+//     var length = response.data.length;
+//
+//
+//     for ( var i = 0; i < length; i++) {
+//         var img = $('<img>');
+//         img.attr('src', response.data[i].images.fixed_height.url);
+//         images.append(img);
+//     }
+// });
 
-}).done(function(response) {
-    var images = $(".images");
-    var length = response.data.length;
-
-
-    for ( var i = 0; i < length; i++) {
-        var img = $('<img>');
-        img.attr('src', response.data[i].images.fixed_height.url);
-        images.append(img);
-    }
-});
 
 
 // Function for displaying movie data
@@ -61,21 +90,9 @@ function renderButtons() {
 
 
 
-// This function handles events where a movie button is clicked
-$("#add-topic").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var topic = $("#topic-input").val().trim();
-
-    // Adding movie from the textbox to our array
-    topic.push(topics);
-
-    // Calling renderButtons which handles the processing of our movie array
-    renderButtons();
-});
 
 // Adding a click event listener to all elements with a class of "movie"
-$(document).on("click", ".topics" );
+//$(document).on("click", ".topics" );
 
 // Calling the renderButtons function to display the initial buttons
 renderButtons();
