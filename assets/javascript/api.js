1450081ed6b28ba200e2546998d44174fac1e06b
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-06-04 17:10:01 -0700

    commit

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -9,11 +9,19 @@
 var topics = ["Movies", "TV Shows", "Entertainment", "Politics", "Science", "Military",
     "Fashion", "Music", "Pop Culture", "Sports"];
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?rating(g pg, pg-13, r)&limit=12&q="
+
+var queryURL = "http://api.giphy.com/v1/gifs/search?rating(g,pg,pg-13,r)&limit=12&q="
 var api_key = "&api_key=dc6zaTOxFJmzC"
+
+
+//host api.giphy.com
+//path v1/gifs/search  -  search endpoint
+// q= any term or phrase https://api.giphy.com/v1/gifs/search?rating(g%20pg,%20pg-13,%20r)&limit=12&q=&api_key=dc6zaTOxFJmzC
+// so within url after q=, you can select anything you would like to search for
 
 $(document).ready(function(){
     renderButtons();
+
     $("#topicBtn").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
@@ -26,9 +34,11 @@ $(document).ready(function(){
         renderButtons();
     });
 
+
     $(".topic").click(function(){
         var btn = $(this); //now we know what button they pressed
-        var txt = $(this).text(); //I think? Trying to get the button's text
+        var txt = $(this).text(); // Trying to get the button's text
+
 
         var query = queryURL + txt + api_key;
         console.log(query); //to test...
@@ -38,30 +48,13 @@ $(document).ready(function(){
             method: 'GET'
         }).done(function(response){
             console.log('ajax finished');
+
+            var images = $(".images");
+            var length = response.data.length;
+
         });
     });
 });
-
-
-//
-// $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-//     //.done(function(response) {
-//     //console.log(response.data[0].embed_url);
-//
-// }).done(function(response) {
-//     var images = $(".images");
-//     var length = response.data.length;
-//
-//
-//     for ( var i = 0; i < length; i++) {
-//         var img = $('<img>');
-//         img.attr('src', response.data[i].images.fixed_height.url);
-//         images.append(img);
-//     }
-// });
-
 
 
 // Function for displaying movie data
@@ -90,9 +83,26 @@ function renderButtons() {
 
 
 
+//
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+//     //.done(function(response) {
+//     //console.log(response.data[0].embed_url);
+//
+// }).done(function(response) {
+//     var images = $(".images");
+//     var length = response.data.length;
+//
+//
+//     for ( var i = 0; i < length; i++) {
+//         var img = $('<img>');
+//         img.attr('src', response.data[i].images.fixed_height.url);
+//         images.append(img);
+//     }
+// });
+
 
 // Adding a click event listener to all elements with a class of "movie"
 //$(document).on("click", ".topics" );
 
-// Calling the renderButtons function to display the initial buttons
-renderButtons();
